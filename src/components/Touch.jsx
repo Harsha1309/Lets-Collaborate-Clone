75e4de1b218465e70e67731f25c79db06ea3970e
Harsha1309 <harshyadav1393@gmail.com>
2023-05-13 21:27:46 +0530

    Assignment completed

--- src/components/Touch.jsx
+++ src/components/Touch.jsx
@@ -5,7 +5,7 @@ const Touch = () => {
   return (
     <>
     <div className='my-5'>
-    <button className="btn touch" style={{width:'200px',cursor:'pointer'}}> <a className='text-decoration-none ' href="https://www.xivtech.io./contact"> Get in touch</a></button>
+    <button className="btn touch" style={{width:'300px',cursor:'pointer'}}> <a className='text-decoration-none ' href="https://www.xivtech.io./contact"> Get in touch</a></button>
     </div>
     </>
   )
